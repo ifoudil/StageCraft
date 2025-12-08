c12a5cb1d7a872f5866bca0c1813ffa37984c4f9
Ludivine Levallois <levallois.ludivine@gmail.com>
2025-12-08 19:36:46 +0100

    feat: login and logout now work

--- stagecraft/applicompte/urls.py
+++ stagecraft/applicompte/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from applicompte import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='applicompte/login.html'), name='login'),
-
+    path('logout/', views.deconnexion, name="logout"),
+    path('connexion/', views.connexion)
 ]
