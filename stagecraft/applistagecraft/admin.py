f97e0890573e1e0398b4601da231d59cd857470e
Ludivine Levallois <levallois.ludivine@gmail.com>
2025-12-08 20:49:19 +0100

    feat: add access to all current models to the superuser in the admin page

--- stagecraft/applistagecraft/admin.py
+++ stagecraft/applistagecraft/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from applistagecraft.models import Regions, EtatsOffres, Offre, Departements
+admin.site.register(Regions)
+admin.site.register(EtatsOffres)
+admin.site.register(Offre)
+admin.site.register(Departements)
